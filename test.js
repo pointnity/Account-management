874823f5c581b5695283f80946f88f4b3eb26172
Pointity Network <38712949+pointnity@users.noreply.github.com>
2018-06-02 18:03:42 +0800

    Update test.js

--- test.js
+++ test.js
@@ -5,3 +5,9 @@ var index = require('./index')
 
 var PrivateKeychain = index.PrivateKeychain,
     PublicKeychain = index.PublicKeychain
+
+var HDPrivateKey = index.HDPrivateKey,
+    HDPublicKey = index.HDPublicKey,
+    PrivateKey = index.PrivateKey,
+    PublicKey = index.PublicKey,
+    Address = index.Address
